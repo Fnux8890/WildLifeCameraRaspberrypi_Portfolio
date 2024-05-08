272cf3dfb902ad9e17d2d7169388b9868ae58ce7
Jeppe M <jeppe.fogh@hotmail.com>
2024-05-08 13:08:23 +0200

    Update rsynchLoop.bash
    Fixed

--- drone/rsynchLoop.bash
+++ drone/rsynchLoop.bash
@@ -13,16 +13,28 @@ current_ssid() {
     echo "$ssid"
 }
 
-# Function to perform rsync operation
-sync_images() {
-    rsync -avz  "$REMOTE_PATH" "$LOCAL_PATH"
+# Function to perform rsync operation to sync images from remote to local
+sync_images_to_local() {
+    rsync -avz "$REMOTE_PATH" "$LOCAL_PATH"
+}
+
+# Function to update JSON metadata using Python
+update_json_metadata() {
+    find "$LOCAL_PATH" -type f -name '*.json' -exec python3 update_json.py {} \;
+}
+
+# Function to sync modified files back to remote
+sync_back_to_remote() {
+    rsync -avz "$LOCAL_PATH"/ "$REMOTE_PATH"
 }
 
 # Infinite loop to check the connection and sync images
 while true; do
     if [[ "$(current_ssid)" == "$TARGET_SSID" ]]; then
         echo "Connected to $TARGET_SSID. Starting image synchronization..."
-        sync_images
+        sync_images_to_local
+        update_json_metadata
+        sync_back_to_remote
         echo "Synchronization completed."
     else
         echo "Not connected to $TARGET_SSID. No action taken."
