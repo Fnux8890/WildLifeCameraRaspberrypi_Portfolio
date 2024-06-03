df4ab2908ee65400b6bf91bf59ca4e3bd60a6484
Krarup214 <krarup214@gmail.com>
2024-06-03 18:27:03 +0200

    Update mqtt_pico_event.sh
    added missing from rpi

--- pi/mqtt_pico_event.sh
+++ pi/mqtt_pico_event.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPT_PATH="./take_photo.sh external"
-
 mosquitto_sub -d -h localhost -p 1883 -u jaflo18 -P avd85hfk -t command | while read payload
 do
     echo "Recived message: $payload"
